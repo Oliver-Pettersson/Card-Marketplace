4ea3193c4dfc22c92c6858248f1bb345cffb9c5e
jannik-boeppli <jannik.boeppli@noseryoung.ch>
2023-01-24 09:12:01 +0100

    added endpoint to get all cards from a user collection

--- backend/src/main/java/com/markteplace/domain/card/CardController.java
+++ backend/src/main/java/com/markteplace/domain/card/CardController.java
@@ -4,8 +4,17 @@ import com.markteplace.core.generic.AbstractEntityController;
 import com.markteplace.core.generic.AbstractEntityService;
 import com.markteplace.core.generic.DTOMapper;
 import com.markteplace.domain.card.dto.CardDTO;
+import com.markteplace.domain.carduser.CardUserService;
+import com.markteplace.domain.carduser.dto.CardCollectionFromUserDTO;
+import com.markteplace.domain.user.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/card")
@@ -14,5 +23,11 @@ public class CardController extends AbstractEntityController<Card, CardDTO> {
         super(service, mapper);
     }
 
+    @GetMapping("/")
+    public ResponseEntity<Collection<CardDTO>> findById() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Collection<Card> dm = ((CardService) service).getCardCollectionFromCurrentUser(user);
 
+        return new ResponseEntity<>(mapper.toDTOs(dm), HttpStatus.OK);
+    }
 }
--- backend/src/main/java/com/markteplace/domain/card/CardService.java
+++ backend/src/main/java/com/markteplace/domain/card/CardService.java
@@ -2,6 +2,10 @@ package com.markteplace.domain.card;
 
 import com.markteplace.core.generic.AbstractEntityService;
 import com.markteplace.domain.cardmarketoffer.CardMarketOffer;
+import com.markteplace.domain.user.User;
+
+import java.util.Collection;
 
 public interface CardService extends AbstractEntityService<Card> {
+    Collection<Card> getCardCollectionFromCurrentUser(User user);
 }
