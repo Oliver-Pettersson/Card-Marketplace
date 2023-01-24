a00f37527c45b6477bb9616c7740f4f373960e1b
andrinklarer <andrin.klarer@noseryoung.ch>
2023-01-24 09:16:03 +0100

    Disable endpoints that the user should not be able to access
    CardTradeOfferController, CardTransactionController, CardUserController, DeckController

--- backend/src/main/java/com/markteplace/domain/cardtradeoffer/CardTradeOfferController.java
+++ backend/src/main/java/com/markteplace/domain/cardtradeoffer/CardTradeOfferController.java
@@ -1,16 +1,29 @@
 package com.markteplace.domain.cardtradeoffer;
 
+import com.markteplace.core.error.UsernameAlreadyExistsException;
 import com.markteplace.core.generic.AbstractEntityController;
 import com.markteplace.core.generic.AbstractEntityService;
 import com.markteplace.core.generic.DTOMapper;
+import com.markteplace.domain.card.dto.CardDTO;
 import com.markteplace.domain.cardtradeoffer.dto.CardTradeOfferDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/card-trade-offer")
 public class CardTradeOfferController extends AbstractEntityController<CardTradeOffer, CardTradeOfferDTO> {
     public CardTradeOfferController(AbstractEntityService<CardTradeOffer> service, DTOMapper<CardTradeOffer, CardTradeOfferDTO> mapper) {
         super(service, mapper);
+    }
+
+    /**
+     * Since it should not be possible to update card trade offers this method is "disabled"
+     *
+     * @return always null
+     */
+    @Override
+    @PutMapping("/not-available")
+    public ResponseEntity<CardTradeOfferDTO> updateById(@PathVariable String ignore, @RequestBody CardTradeOfferDTO dto) throws UsernameAlreadyExistsException {
+        return null;
     }
 }
--- backend/src/main/java/com/markteplace/domain/carduser/CardUserController.java
+++ backend/src/main/java/com/markteplace/domain/carduser/CardUserController.java
@@ -1,16 +1,52 @@
 package com.markteplace.domain.carduser;
 
+import com.markteplace.core.error.UsernameAlreadyExistsException;
 import com.markteplace.core.generic.AbstractEntityController;
 import com.markteplace.core.generic.AbstractEntityService;
 import com.markteplace.core.generic.DTOMapper;
+import com.markteplace.domain.cardtransaction.dto.CardTransactionDTO;
 import com.markteplace.domain.carduser.dto.CardUserDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/card-user")
 public class CardUserController extends AbstractEntityController<CardUser, CardUserDTO> {
     public CardUserController(AbstractEntityService<CardUser> service, DTOMapper<CardUser, CardUserDTO> mapper) {
         super(service, mapper);
+    }
+
+
+    /**
+     * Since it should not be possible to create new card users this method is "disabled"
+     *
+     * @return always null
+     */
+    @Override
+    @GetMapping("/not-available")
+    public ResponseEntity<CardUserDTO> create(@RequestBody CardUserDTO dto) {
+        return null;
+    }
+
+    /**
+     * Since it should not be possible to update card users this method is "disabled"
+     *
+     * @return always null
+     */
+    @Override
+    @PutMapping("/not-available")
+    public ResponseEntity<CardUserDTO> updateById(@PathVariable String ignore, @RequestBody CardUserDTO dto) throws UsernameAlreadyExistsException {
+        return null;
+    }
+
+    /**
+     * Since it should not be possible to delete card users this method is "disabled"
+     *
+     * @return always null
+     */
+    @Override
+    @DeleteMapping("/not-available")
+    public ResponseEntity<Void> deleteById(@PathVariable String ignore) {
+        return null;
     }
 }
